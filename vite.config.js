60abdf15d839ad0321eff954f09758b17e4c63fa
shamsur-rahman-rifat <shamsur30rahman@gmail.com>
2025-05-16 09:37:57 +0600

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/code-blog"
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+  base: process.env.VITE_BASE_PATH || "/code-blog",
 });
